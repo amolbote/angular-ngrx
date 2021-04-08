6fe16cd0f3c1a363bd5b360e85e8a6571b277000
amolbote <amol.a.bote@gmail.com>
2021-04-08 22:31:39 +0530

    2 files modified

--- src/app/counter-buttons/counter-buttons.component.ts
+++ src/app/counter-buttons/counter-buttons.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmiter } from "@angular/core";
 
 @Component({
-  selector: 'app-counter-buttons',
-  templateUrl: './counter-buttons.component.html',
-  styleUrls: ['./counter-buttons.component.css']
+  selector: "app-counter-buttons",
+  templateUrl: "./counter-buttons.component.html",
+  styleUrls: ["./counter-buttons.component.css"]
 })
 export class CounterButtonsComponent implements OnInit {
+  @Output() increment = new EventEmiter();
+  @Output() decrement = new EventEmiter();
+  @Output() reset = new EventEmiter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  incrementClicked() {
+    this.increment.emit();
   }
 
+  decrementClicked() {
+    this.decrement.emit();
+  }
+
+  resetClicked() {
+    this.reset.emit();
+  }
 }

94c0517d9578df852ea7304572c9368b3cfdfe43
amolbote <amol.a.bote@gmail.com>
2021-04-09 11:09:16 +0530

    2 files added

---
+++ src/app/store/counter.reducer.ts
@@ -0,0 +1,26 @@
+import { state, action, createReducer, on } from "@ngrx/store";
+import { initialState } from "./counter.state";
+function _counterReducer = createReducer(initialState, 
+  on('increment', state => {
+    return {
+      ...state,
+      counter : state.counter + 1;
+    }
+  }),
+on('decrement', state => {
+    return {
+      ...state,
+      counter : state.counter - 1;
+    }
+  }),
+  on('reset', state => {
+    return {
+      ...state,
+      counter : 0;
+    }
+  })
+  );
+
+export function counterReducer(state, actions) {
+  return _counterReducer(state, action);
+}
